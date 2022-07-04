ff89b9418aa68b2ad0596aab3be470ab743f4a41
Rotten-LKZ <Rotten_LKZ@163.com>
2022-07-04 18:16:44 +0800

    fix: avoid using localStorage in src

--- apis/index.ts
+++ apis/index.ts
@@ -4,6 +4,13 @@ import image from './server/image'
 
 const config: ApiConfig = {
   apiEnv: 'SERVER',
+  set: (key, value) => {
+    localStorage.setItem(key, value)
+  },
+  get: async (key) => {
+    const res = localStorage.getItem(key)
+    return res || undefined
+  },
 }
 
 window.API = { user, article, image, config }
