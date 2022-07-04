6241519ea1c829a3079ba4a6d09c8a001e88960d
Rotten-LKZ <Rotten_LKZ@163.com>
2022-07-04 20:13:27 +0800

    feat: github APIs

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,7 @@ declare module '@vue/runtime-core' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElDivide: typeof import('element-plus/es')['ElDivide']
     ElDivider: typeof import('element-plus/es')['ElDivider']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
